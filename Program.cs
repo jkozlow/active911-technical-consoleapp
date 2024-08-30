fe5da14083b217b7281fff6b4a0bace5907f49e9
Josh <Josh.Kozlowski81@outlook.com>
2024-08-30 07:13:57 -0700

    project creation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace active911_technical_consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
